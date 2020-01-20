c2d093b330de35ffe18dc8a694e098dbf8f467c8
taratara0719 <taratara0719@gmail.com>
2020-01-20 15:35:46 +0900

    garch

---
+++ garch_datagenerator.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+import math
+
+sns.set(style='darkgrid')
+
+def main():
+    r = 1  # number of source components
+
+    np.random.seed(0)
+    #  system model
+    #  AR(2)model parameter
+    phi1 = np.random.normal(0, 0.3)
+    phi2 = np.random.normal(0, 0.3)
+    sigma = 1
+    F = np.mat([[phi1, phi2], [1, 0]])
+    Q = np.mat([[sigma, 0], [0, 0]])
+
+    #  observation model
+    c = 1
+    H = np.mat([c, 0])
+    R = 1
+
+    #  test data generating
+    T = 200  # number of sampling
+    x = np.mat(np.random.normal(0, 0.3, (2, 1)))
+    y = 0
+    
+    X = [x]
+    Y = [y]
+    sigma_ = [sigma]
+    nu = 1
+    nu_ = [nu]
+    alpha = 2
+    beta = 1
+    p = 2
+    q = 2
+
+    for i in range(T-1):
+        Q = np.mat([[sigma, 0], [0, 0]])
+        x = F @ x + np.random.multivariate_normal([0, 0], Q, 1).T
+        #x_ = np.hstack((X, x))
+        X.append(x)
+        y = H @ x + np.random.normal(0, R)
+        Y.append(y)
+        nu = np.random.normal(0, 1, )
+        nu_.append(nu)
+        sigma_.append(sigma)
+        if i >= p and i >= q:
+                ar = 0
+                ma = 0
+                for j in range(1, p):
+                    ar += alpha * math.log(sigma_[-j])
+                for k in range(1, q):
+                    ma += beta * math.log((nu_[-k])**2) 
+                s = math.log(sigma**2) + ar + ma
+                sigma = np.exp(s)
+    
+
+    plt.subplot(3, 1, 1)
+    #for i in range(r):
+        #plt.plot(x[:,i], label='x{}'.format(i+1))
+    a, b = np.array(np.concatenate(X, axis=1))
+    plt.plot(a, label='x')
+    plt.legend()
+    
+    plt.subplot(3, 1, 2)
+    plt.plot(Y, label='y', color='red')
+    plt.legend()
+
+    plt.subplot(3, 1, 3)
+    plt.plot(sigma_, label='sigma', color='orange')
+    plt.xlabel('time')
+    plt.legend()
+
+
+    plt.savefig('../fig/garch_states.png')
+    print('fig saved')
+    
+    np.savetxt(fname='../data/garch_hid_states.txt',fmt='%.5f', X=X, delimiter=',')
+    np.savetxt(fname='../data/garch_obs_states.txt',fmt='%.5f', X=Y, delimiter=',')
+    print('data saved')
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
--- garch_kalman_filter.py
+++ garch_kalman_filter.py
@@ -8,18 +8,21 @@ sns.set(style='darkgrid')
 
 class kf_lse(object):
 
-    def __init__(self, phi1, phi2, p, q, T):
+    def __init__(self, phi1, phi2, p, q, alpha, beta, T):
         self.phi1 = phi1
         self.phi2 = phi2
         self.p = p
         self.q = q
         self.T = T
+        self.alpha = alpha
+        self.beta = beta
     
     #  初期値
     def init_initial(self, y):
         self.x = np.mat([[0.2936214], [0.67226796]])
         self.P = np.zeros((2, 2))
         self.sigma = 1
+        self.sigma0 = 1
         self.Q = np.mat([[self.sigma, 0], [0, 0]])
         self.F = np.mat([[self.phi1, self.phi2], [1, 0]])
         self.H = np.mat([1, 0])
@@ -29,6 +32,7 @@ class kf_lse(object):
         self.c = 0
         self.d = 0
         self.e = 0
+    
 
     #  最小二乗法
     def lse(self, x):
@@ -52,9 +56,9 @@ class kf_lse(object):
         self.X = [self.x]
         self.phi1_ = [self.phi1]
         self.phi2_ = [self.phi2]
-        K_ = [self.K]
-        sigma_ = [self.sigma]
-        nu_ = [self.nu]
+        K_ = [K]
+        self.sigma_ = [self.sigma]
+        nu_ = [nu]
         
         for i in tqdm(range(self.T-1)):
             # prediction
@@ -67,17 +71,17 @@ class kf_lse(object):
             K = P_ @ self.H.T / S
             self.x = x_ + K * yi
             self.P = P_ - K * self.H @ P_
-            self.nu = self.Q - self.Q @ self.H.T /S @ self.H @ self.Q  + K * yi * yi @ K.T
+            nu = self.Q - self.Q @ self.H.T /S @ self.H @ self.Q  + K * yi * yi @ K.T
 
             #  sigma filtering
             if i >= self.p and i >= self.q:
                 ar = 0
                 ma = 0
                 for j in range(1, self.p):
-                    ar += self.alpha * math.log(sigma_[-j])
+                    ar += self.alpha * math.log(self.sigma_[-j])
                 for k in range(1, self.q):
                     ma += self.beta * math.log(nu_[-k]) 
-                s = math.log(sigma0**2) + ar + ma
+                s = math.log(self.sigma0**2) + ar + ma
                 self.sigma = math.log(s)
             
             self.X.append(self.x)
@@ -87,76 +91,13 @@ class kf_lse(object):
             self.phi1_.append(self.phi1)
             self.phi2_.append(self.phi2)
         
-            yi_.append(yi)
             K_.append(K)
-            Q_.append(Q)
-            sigma_.append(sigma)
+            self.Q_.append(self.Q)
+            self.sigma_.append(self.sigma)
             nu_.append(nu)
 
-
         return self
-def kf(T, Y, sigma0, x0, P0, F, H, R, p, q, alpha, beta):
-    x = x0
-    P = P0
-    sigma = sigma0
-    nu = 0
-    yi = 0
-    K = 0
-
-    Q = np.mat([[sigma, 0], [0, 0]])
-    X = [x]  # hidden states
-    yi_ = [yi]  # prediction error
-    K_ = [K]
-    Q_ = [Q]
-    sigma_ = [sigma]
-    nu_ = [nu]
-
-    for i in range(T-1):
-
-        # prediction
-        x_ = F @ x            # priori state estimate
-        P_ = Q + F @ P @ F.T  # priori estimate covariance 
-        
-        #filtering
-        yi = Y[i+1] - H @ x_    # innovation
-        S = H @ P_ @ H.T + R    # innovation covariance
-        K = P_ @ H.T / S        # Optimal Kalman gain
-        x = x_ + K * yi         # posteriori state estimate 
-        P = P_ - K * H @ P_     # posteriori estimate covariance
-        nu = Q - Q @ H.T /S @ H @ Q  + K * yi * yi @ K.T
-        
-        if i >= p and i >= q:
-            ar = 0
-            ma = 0
-            for j in range(1, p):
-                ar += alpha * math.log(sigma_[-j])
-            for k in range(1, q):
-                ma += beta * math.log(nu_[-k]) 
-            s = math.log(sigma0**2) + ar + ma
-            sigma = math.log(s)
-            ar = 0
-            ma = 0
-            
-
-        X.append(x)
-        yi_.append(yi)
-        K_.append(K)
-        Q_.append(Q)
-        sigma_.append(sigma)
-        nu_.append(nu)
-
-        Q = np.mat([[sigma_[-1], 0], [0, 0]])
-
     
-    print("yi", yi)
-    print("x_", x_)
-    print("P", P)
-    print("P_", P_)
-    print("K", K)
-    print("H", H)
-    
-
-    return X
 
 def main():
 
@@ -188,7 +129,7 @@ def main():
     alpha = 0
     beta = 0.5
 
-    X = kf(T, y, sigma0, x0, P0, F, H, R, p, q, alpha, beta)
+    X = kf_lse(0.529, 0.120, 0, 1, 0, 0.5, len(x))
     
     
     plt.plot(x[:, 1], label='x')
